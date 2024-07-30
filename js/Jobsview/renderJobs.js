1b38fa230d4608e8c4caac76ba223c5c80d4beb6
Aseel Abu saleh <165641652+aseelas21@users.noreply.github.com>
2024-07-31 00:57:34 +0300

    fixed rendejobs.js

--- js/Jobsview/renderJobs.js
+++ js/Jobsview/renderJobs.js
@@ -1,20 +1,42 @@
 export function renderJobs(jobs) {
-    const getalljobs = document.getElementById('getalljobs');
+    const allJobsMenu = document.getElementById('alljobsmenu');
+    const jobsForYouMenu = document.getElementById('jobsforyoumenu');
 
     if (jobs.length === 0) {
         console.log('No jobs available');
         return;
     }
 
-    getalljobs.innerHTML = '';
+    allJobsMenu.innerHTML = '';
+    jobsForYouMenu.innerHTML = '';
 
-   for(let i = 0 ; i < jobs.length ; i++){
+    jobs.forEach(job => {
+        let jobElement = document.createElement('li');
+        jobElement.className = 'job-item';
 
-    let jobElement = document.createElement('div');
-    jobElement.className = 'job-item';
+        let jobText = document.createElement('span');
+        jobText.textContent = `${job.job_title} - ${job.company_title}`;
 
-    jobElement.textContent = `${jobs[i].job_title} - ${jobs[i].company_title}`;
+        let arrowIcon = document.createElement('i');
+        arrowIcon.className = 'fa-solid fa-arrow-right';
 
-    getalljobs.appendChild(jobElement);
-   }
+        jobElement.appendChild(jobText);
+        jobElement.appendChild(arrowIcon);
+        allJobsMenu.appendChild(jobElement);
+
+        if (job.isForYou) {
+            let jobForYouElement = document.createElement('li');
+            jobForYouElement.className = 'job-item';
+
+            let jobForYouText = document.createElement('span');
+            jobForYouText.textContent = `${job.job_title} - ${job.company_title}`;
+
+            let arrowIconForYou = document.createElement('i');
+            arrowIconForYou.className = 'fa-solid fa-arrow-right';
+
+            jobForYouElement.appendChild(jobForYouText);
+            jobForYouElement.appendChild(arrowIconForYou);
+            jobsForYouMenu.appendChild(jobForYouElement);
+        }
+    });
 }
