51e38e9ab8d3918048036fe03ebdf2d4c06328e7
Aseel Abu saleh <165641652+aseelas21@users.noreply.github.com>
2024-08-01 15:08:28 +0300

    deleted files

--- js/postajob.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('post-job-form').addEventListener('submit', function(event) {
-    var checkboxes = document.querySelectorAll('input[name="work-type"]');
-    var checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked);
-    
-    if (!checkedOne) {
-        alert('Please select at least one type of work.');
-        event.preventDefault();
-    }
-});
