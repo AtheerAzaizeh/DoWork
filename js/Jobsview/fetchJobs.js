8497813f291748f06fbdfcb8e3520d3b4eca1735
Aseel Abu saleh <165641652+aseelas21@users.noreply.github.com>
2024-08-03 22:33:44 +0300

    deleted files

--- js/Jobsview/fetchJobs.js
+++
@@ -1,9 +0,0 @@
-export async function fetchJobs() {
-    try {
-        const response = await fetch('https://onlybackend-wgcr.onrender.com/api/all-jobs');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error.message);
-    }
-}
